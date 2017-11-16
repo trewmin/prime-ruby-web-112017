c20b9037c374530ff37621184c403d28e8c10168
Truman <trumanlahr@gmail.com>
2017-11-16 12:05:51 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(num)
+  if num <= 2
+    return false
+  else
+    array=(2...(num-1)).to_a
+    array.each do |int|
+    if num % int == 0
+      return false
+    else
+    end
+    end
+    return true
+  end
+end# Add  code here!
